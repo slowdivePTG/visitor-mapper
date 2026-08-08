75d680f30ee4574487c148d511972ac6d204fa5c
Chang Liu <ptg.cliu@u.northwestern.edu>
2026-08-08 09:17:51 +0800

    update: light mode hue

--- map_render.py
+++ map_render.py
@@ -246,8 +246,8 @@ var themes = {{
         stop3: [0.34, 0.29, 0.41],
         tooltipBg: "#9ca0b0",
         tooltipText: "#e6e9ef",
-        currentDot: "#464057",
-        currentDotRgba: "70, 64, 87",
+        currentDot: "#8839ef",
+        currentDotRgba: "136, 57, 239",
     }}
 }};
 
